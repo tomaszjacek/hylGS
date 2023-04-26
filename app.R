5f34607605670b68d6dbcba69bf17360590b149b
tomaszjacek <tomasz_jacek@wp.pl>
2023-04-26 23:07:45 +0200

    drobnica

--- app.R
+++ app.R
@@ -3,40 +3,64 @@ library("shiny")
 ui <- bootstrapPage(
 
 
-  verbatimTextOutput("localTagVersion"),
-  verbatimTextOutput("localBranchList"),
-  verbatimTextOutput("remoteBranchList"),
-  selectInput(inputId = "mainWin_tagList",
-              label = "chose main branch version to download",
+  verbatimTextOutput("localTagVersion_string_mainPanel"),
+  verbatimTextOutput("localBranchList_string_mainPanel"),
+  verbatimTextOutput("remoteBranchList_string_mainPanel"),
+  
+  verbatimTextOutput("pullBranch_string_mainPanel"),
+  selectInput(inputId = "remoteBranches_list_mainPanel",
+              label = "chose remote branch to download",
+              "Names"),
+  actionButton("pull_button_mainPanel", "download hylGS"),
+  verbatimTextOutput("resultPull"),
+  
+  selectInput(inputId = "localBranchesTags_list_mainPanel",
+              label = "chose branch:tag version to switch",
               "Names"),
   actionButton("pull", "download hylGS"),
-  verbatimTextOutput("result"),
+  verbatimTextOutput("resultPull"),
 )
 
 server <- function(input, output, session) {
+  
+  #check hylGS's branches present in local repository
+  localBranchList<-system2("git", "branch -l", stdout = TRUE, stderr = TRUE)
+  localBranchListClean <- unique(sub("^.*/", "", localBranchList))
+  localBranchListClean <- sub("\\* ", "", localBranchListClean)
+  output$localBranchList_string <- renderText(paste0("local hylGS branches: ",paste(localBranchListClean, collapse=", ")))  
+  
+  
+  #check hylGS's branches present in remote repository (https://github.com/tomaszjacek/hylGS.git)
+  remoteBranchList<-system2("git", "branch -r", stdout = TRUE, stderr = TRUE)
+  remoteBranchListClean <- unique(sub("^.*/", "", remoteBranchList))
+  output$remoteBranchList_string <- renderText(paste0("remote hylGS branches: ",paste(remoteBranchListClean, collapse=", ")))
+  #update selection list on webpage
+  updateSelectizeInput(session, "remoteBranches_list_mainPanel",choices = remoteBranchListClean)
+  
+  
   #list all tags in main branch on remote repository
   tagList<-system2("git", "show-ref --tags", stdout = TRUE, stderr = TRUE)
   #clean the strings like "4d722717d2579f02b2f9c311513d355e36774aef refs/tags/v0.0.0.1" "b06d7e1b9cdd4a88a00989ebec75181bef6c6043 refs/tags/v0.0.0.2"  to "v0.0.0.1" "v0.0.0.2" 
   tagListClean <- sub("^.*/", "", tagList)
+  #update selection list on webpage
+  updateSelectizeInput(session, "localBranchesTags_list_mainPanel",choices = tagListClean)
   
-  updateSelectizeInput(session, "mainWin_tagList",choices = tagListClean)
-  
-  result <- eventReactive(input$pull, {
+  resultPull <- eventReactive(input$pull_button_mainPanel, {
     system2("git", "pull origin main", stdout = TRUE, stderr = TRUE)
   })
 
-  output$localTagVersion <- renderText({paste0("local hylGS version: ",system2("git", "describe --tags", stdout = TRUE, stderr = TRUE))})
+  output$localTagVersion_string <- renderText({paste0("local hylGS version: ",system2("git", "describe --tags", stdout = TRUE, stderr = TRUE))})
   
   
-  remoteBranchList<-system2("git", "branch -r", stdout = TRUE, stderr = TRUE)
-  remoteBranchListClean <- unique(sub("^.*/", "", branchList))
-  output$remoteBranchList <- renderText(paste0("remote hylGS branches: ",paste(remoteBranchListClean, collapse=", ")))
+
   
-  localBranchList<-system2("git", "branch -l", stdout = TRUE, stderr = TRUE)
-  localBranchListClean <- unique(sub("^.*/", "", branchList))
-  output$localBranchList <- renderText(paste0("local hylGS branches: ",paste(localBranchListClean, collapse=", ")))  
+
   
+  localVsRemoteBranchesDiff <- localBranchList[!(localBranchList %in% remoteBranchList)]
   
+  if(length(localVsRemoteBranchesDiff>0)){
+    output$ <- renderText(paste0("local hylGS branches: ",paste(localBranchListClean, collapse=", ")))
+  }
   
   #output$user <- renderTable({
   #  info <- Sys.info()
